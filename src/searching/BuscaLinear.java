4fe7239ad3ad3243a6e8460bee17eab65a20a78a
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-01-18 19:17:23 -0300

    feat: implement linear search (iterative and recursive)

---
+++ src/searching/BuscaLinear.java
@@ -0,0 +1,28 @@
+package searching;
+
+import model.Estudante;
+
+public class BuscaLinear {
+    public static Estudante buscaLinearInterativa(Estudante[] vetor, int matricula){
+        int n = vetor.length;
+
+        for (Estudante estudante : vetor) {
+            if (estudante.getMatricula() == matricula) {
+                return estudante;
+            }
+        }
+        return null;
+    }
+
+    public static Estudante buscaLinearRecursiva(Estudante[] vetor, int matricula, int indice){
+        if (indice >= vetor.length) {
+            return null;
+        }
+
+        if (vetor[indice].getMatricula() == matricula) {
+            return vetor[indice];
+        }
+
+        return buscaLinearRecursiva(vetor, matricula, indice + 1);
+    }
+}
