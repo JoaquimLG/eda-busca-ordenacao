200906492722cc1110b69f2b1884dff230da7110
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-01-19 08:33:00 -0300

    feat: implement binary search (iterative and recursive)

---
+++ src/searching/BuscaBinaria.java
@@ -0,0 +1,46 @@
+package searching;
+
+import model.Estudante;
+
+public class BuscaBinaria {
+    public static Estudante buscaBinariaInterativa (Estudante[] vetor, int matricula){
+        int inicio = 0;
+        int fim = vetor.length - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (vetor[meio].getMatricula() == matricula){
+                return vetor[meio];
+            }
+
+            else if(vetor[meio].getMatricula() < matricula){
+                inicio = meio + 1;
+            }
+            else{
+                fim = meio - 1;
+            }
+        }
+
+        return null;
+    }
+
+    public static Estudante buscaBinariaRecursiva (Estudante[] vetor, int matricula, int inicio, int fim){
+        if (inicio > fim) {
+            return null;
+        }
+
+        int meio = (inicio + fim) / 2;
+
+        if(vetor[meio].getMatricula() == matricula){
+            return vetor[meio];
+        }
+
+        else if(vetor[meio].getMatricula() < matricula){
+            return buscaBinariaRecursiva(vetor,matricula, meio + 1, fim);
+        }
+        else{
+            return buscaBinariaRecursiva(vetor, matricula, inicio, meio - 1);
+        }
+    }
+}
